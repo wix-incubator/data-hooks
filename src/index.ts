4805e0c64bb5d25e586df2f8a8f08876a0250780
Roman Shevchuk <romansh@wix.com>
2021-05-16 16:30:11 +0300

    feature: export types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export { dataHooks } from './dataHooks';
 export { withDataHooks } from './withDataHooks';
+export {
+  DataHooks,
+  DataHookValue,
+  DataHooksGenerator,
+  WithDataHooks,
+} from './types';
